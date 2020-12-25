109906bf9ab83f31c156eb9440b4fc74cdc3d15b
abhijari <abhijariwala13@gmail.com>
2020-12-25 13:42:51 +0530

    add templates

---
+++ QnA/Controllers/BadgesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QnA.Controllers
+{
+    public class BadgesController : Controller
+    {
+        // GET: Badges
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
